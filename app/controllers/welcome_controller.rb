7891d963629b764a93ff4c8265441d0ee35a72f7
wcargen <wcargen@gmail.com>
2013-10-10 11:25:23 -0700

    new rails project hooray

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+	def index
+	end
+
+	def another_index
+		@var = "Some value"
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kittenz::Application.config.secret_token = 'af307d12f8b625002cd137d5e8cbafae3a0c2fb5ab09b92a6a2d36d852507d5b50967475145a7ee92ebfdd2421031cac621a943a585fd2e6a291f683166b05bd'
